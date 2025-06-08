a717cbfbc8a102c87d42b68152b93520e13cf7d1
aaryan9009 <aryanpatil9700@gmail.com>
2025-06-08 20:30:33 +0530

    correct typo

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "ARYAN PATIL",
-  profile: '/profile.png',
+  profile: '/Profile.png',
   designation: "Software Developer",
   description: "My name is ARYAN PATIL. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core expertise lies in web application development, particularly in crafting beautifully designed and interactive web pages. I have a deep interest in creating animated and engaging user experiences that make the web more dynamic and accessible. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'aryanpatil9700@gmail.com',
